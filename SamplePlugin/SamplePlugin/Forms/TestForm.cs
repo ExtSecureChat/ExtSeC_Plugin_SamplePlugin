b6058c50235bee81f6aecd53e03917c66eefc3a1
PsychoPflanze <bluedragonhunter@web.de>
2018-05-12 01:58:19 +0200

    Stuff

---
+++ SamplePlugin/SamplePlugin/Forms/TestForm.cs
@@ -0,0 +1,17 @@
+﻿using PluginLibrary;
+
+namespace SamplePlugin.Forms
+{
+    public partial class TestForm : PluginForm
+    {
+        public override string GetName()
+        {
+            return "TEST_FORM";
+        }
+
+        public TestForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SamplePlugin/SamplePlugin/SamplePlugin.cs
@@ -0,0 +1,31 @@
+﻿using PluginLibrary;
+using SamplePlugin.Forms;
+using System;
+using System.Collections.Generic;
+
+namespace SamplePlugin
+{
+    public class SamplePlugin : GuiPlugin
+    {
+        public override string Name => "SamplePlugin";
+        public override PluginTypes PluginType => PluginTypes.GUI_PLUGIN;
+
+        public override void OnLoad()
+        {
+            Console.WriteLine("Loaded Plugin: " + Name);
+
+            BeforeFormTransitionEventHandler += (PluginForm currentForm, ref string destinationForm) =>
+            {
+                if (destinationForm == PluginHelper.Forms.CONNECT_FORM)
+                {
+                    destinationForm = "TEST_FORM";
+                }
+            };
+        }
+
+        public override void OverrideForms(ref List<PluginForm> forms)
+        {
+            forms.Add(new TestForm());
+        }
+    }
+}
